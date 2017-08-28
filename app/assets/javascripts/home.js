18d496cae7ab5328289e42d57c5e832d234b8c6c
gusholguin <augustin.holguin@gmail.com>
2017-08-28 20:38:06 +0200

    image setup done

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,13 @@
+// require jquery
+
+/*
+$(function() {
+  $('img').on('click', function(){
+    $('img').attr('src', 'http://xczech.com/wp-content/uploads/2015/07/emili-ratajkovski.jpg');
+  })
+});
+*/
+
+$(function() {
+  $('img').show();
+});
